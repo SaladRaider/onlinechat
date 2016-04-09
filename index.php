a43cfef9527deb6a5d618f595adab988126b792c
SaladRaider <fakermansacc@gmail.com>
2016-04-09 09:12:11 -0700

    Added new index and main.css file

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<!-- Code by Peter Veerman -->
+
+<html>
+
+<head>
+	<!-- Website Title -->
+	<title>Arcadia Film Festival</title>
+	
+	<!-- Website Icon -->
+	<link rel="shortcut icon" href="images/logo-tiny.ico" />
+	
+	<!-- CSS Link -->
+	<link rel="stylesheet" type="text/css" media="all" href="main.css" />
+	
+	<!-- Description -->
+	<meta name="description" content="The Arcadia Film Festival will be hosted by the Arcaida Film Club." />
+	
+	<!-- Keywords -->
+	<meta name="keywords" content="film festival, indie films, independent films, arcadia high school, film club" />
+	
+	<!-- Author -->
+	<meta name="author" content="Peter Veerman" />
+	
+	<!-- Character Set -->
+	<meta charset="UTF-8" />
+	
+	<!-- JQuery Reference (Don't worry about this) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" ></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
+
+</head>
+
+
+
+<body>
+
+<!-- Content -->
+<div id="wrapper">
+<span id="faq"></span>
+	<!-- Make Major Changes Inside Here!!! -->
+	<div ng-app="" class="container">
+		<h2>FAQ {{2+2}}</h2>
+		<div style="text-align:left;">
+		<p>
+		<b>Q:</b> May I work with people who are not in the same school or who are not even in school?<br />
+		<b>A:</b> Yes, you may work with anyone you would like as long as you are a high school, middle school or elementry school student.
+		</p>
+		<p>
+		<b>Q:</b> Can I or a group submit more than one video?<br />
+		<b>A:</b> Yes, you may submit as many videos as you would like. Just make sure to pay the $10 fee for each video.
+		</p>
+		<p>
+		<b>Q:</b> Can I submit a video I previously made?<br />
+		<b>A:</b> Yes, you may submit previous work.
+		</p>
+		<p>
+		<b>Q:</b> What type of videos are allowed? Are there categories?<br />
+		<b>A:</b> You may create any type of video you would like. You can create live action, animation, stop motion, 3D animaion, etc. As long as you create a video you should be fine. There are no category prizes this film festival. There are only overall winners.
+		</p>
+		</div>
+	</div>
+</div>
+<!-- End Of Content -->
+
+
+<!-- Footer -->
+<div id="footer">
+</div>
+<!-- End Of Footer -->
+
+</body>
+
+</html>
